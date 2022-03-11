df3dc2d3cfafb42806aae8b2868feee126bc82e6
onskulit <dislaty@mail.com>
2022-03-11 17:41:08 +0300

    feat: add projects layout

---
+++ index.js
@@ -0,0 +1,35 @@
+/* tags
+    Layout: HTML5, CSS3, SASS, BEM, adaptive, flex
+    JS: API, game, player
+*/
+
+const projects = [
+    {
+        imgSrc: './img/projects/potfolio.PNG',
+        deploySrc: 'https://onskulit.github.io/stage0-projects/portfolio/',
+        projectTitle: 'Portfolio',
+        tags: ['JS', 'HTML5', 'CSS3'],
+        githubSrc: 'https://github.com/onskulit/stage0-projects/tree/main/portfolio',
+    },
+    {
+        imgSrc: './img/projects/audio-player.PNG',
+        deploySrc: 'https://onskulit.github.io/stage0-projects/audio-player/',
+        projectTitle: 'Custom Video Player',
+        tags: ['JS', 'HTML5', 'CSS3'],
+        githubSrc: 'https://github.com/onskulit/stage0-projects/tree/main/audio-player',
+    },
+    {
+        imgSrc: './img/projects/movie-app.PNG',
+        deploySrc: 'https://onskulit.github.io/stage0-projects/movie-app/',
+        projectTitle: 'Movie App',
+        tags: ['JS', 'HTML5', 'CSS3'],
+        githubSrc: 'https://github.com/onskulit/stage0-projects/tree/main/movie-app',
+    },
+    {
+        imgSrc: './img/projects/tic-tac-toe.PNG',
+        deploySrc: 'https://onskulit.github.io/stage0-projects/portfolio/',
+        projectTitle: 'Tic Tac Toe',
+        tags: ['JS', 'HTML5', 'CSS3'],
+        githubSrc: 'https://github.com/onskulit/stage0-projects/tree/main/tic-tac-toe',
+    },
+]
